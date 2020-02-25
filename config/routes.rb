de38eba5f7fb63e784edfd743ce9af60cebcd717
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-02-25 15:27:44 +0100

    Adds devise authentication for User class, testing pings and pongs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount_devise_token_auth_for 'User', at: 'auth'
+  namespace :api do
+    namespace :v0 do
+      resources :pings, only: [:index], contraints: { format: 'json'}
+    end
+  end
 end
