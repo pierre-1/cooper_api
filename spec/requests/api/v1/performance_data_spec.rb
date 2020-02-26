3328d6c8bbd444236b5950f6c37f7be85d7b365e
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-26 11:23:29 +0100

    modifying the controller and adding a test to see that only authenticated users can call the post verb

--- spec/requests/api/v1/performance_data_spec.rb
+++ spec/requests/api/v1/performance_data_spec.rb
@@ -1,10 +1,12 @@
-Rspec.describe Api::V1::PerformanceDataController, type: :request do
-  let(:headers) { { HTTP_ACCEPT: 'application/json'} }
+RSpec.describe Api::V1::PerformanceDataController, type: :request do
+  let(:user) { create(:user) }
+  let(:credentials) { user.create_new_auth_token }
+  let(:headers) { { HTTP_ACCEPT: 'application/json'}.merge!(credentials) }
 
   describe 'POST /api/v1/performance_data' do
     before do
-      post 'api/v1/performance_data',
-      perams: {
+      post '/api/v1/performance_data',
+      params: {
           performance_data: {
               data: { message: 'Average' }
           }
