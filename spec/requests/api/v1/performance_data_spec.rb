0512d107835f013c4bf388db14d6c7eddcc79fcb
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-02-26 12:15:20 +0100

    Adds initial tests for sad path in performance_data_spec.rb, currently attempting to return 400 response status

--- spec/requests/api/v1/performance_data_spec.rb
+++ spec/requests/api/v1/performance_data_spec.rb
@@ -22,6 +22,31 @@ RSpec.describe Api::V1::PerformanceDataController, type: :request do
       entry = PerformanceData.last
       expect(entry.data).to eq 'message' => 'Average'
     end
+
+    # describe 'wrong POST /api/v1/performance_data' do
+    #   before do
+    #     post '/api/v1/performance_data',
+    #     params: {
+    #         performance_data: {
+    #             data: { message: 400 }
+    #         }
+    #     },
+    #     headers: headers
+    #   end
+
+    #   it 'returns a 400 response status' do
+    #     expect(response).to have_http_status 400
+    #   end
+
+    #   it 'unsuccessfully creates a data entry' do
+    #     entry = PerformanceData.last
+    #     expect(entry.data).to eq 'message' => 400
+    #   end
+    # end
+
+
+
+
   end
 
   describe 'GET /api/v1/performance_data' do
@@ -45,4 +70,25 @@ RSpec.describe Api::V1::PerformanceDataController, type: :request do
       expect(response_json['entries'].count).to eq 5
     end
   end
+
+  # describe 'GET /api/v1/performance_data' do
+  #   let!(:existing_entries) do
+  #     5.times do
+  #       create(:performance_data,
+  #       data: { message: 'Average' },
+  #       user: user)
+  #     end
+  #   end
+
+  #   it 'returns a 406 response status' do
+  #     expect(response).to have_http_status 406
+  #   end
+
+  #   it 'does not return a collection of performance data' do
+  #     expect(response_json['entries'].count).to eq 5
+  #   end
+
+
+
+
 end
