60442ae64d8f8856211c92b72d69b67805123a39
Emma_Thalen <thlalen.emma@gmail.com>
2020-02-26 11:34:19 +0100

    adding specs for the get verb and also adding that to the routes and the controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/v1/auth', skip: [:omniauth_callbacks]
   namespace :api do
     namespace :v1, defaults: { format: :json } do
-      resources :performance_data, only: [:create]
+      resources :performance_data, only: [:create, :index]
     end
   end
 end
