bf8368b1575bf050b1cd5bab83a2bc82c96e816e
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-02-26 10:17:14 +0100

    Adds performance data as a hstore and migrates db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
-# frozen_string_literal: true
-
 class User < ActiveRecord::Base
   extend Devise::Models
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+  has_many :performance_data, class_name: 'PerformanceData'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,5 +30,9 @@ RSpec.describe User, type: :model do
         it { is_expected.to allow_value(email).for(:email) }
       end
     end
+
+    describe 'Relations' do
+      it { is_expected.to have_many :performance_data }
+    end
   end
 end
